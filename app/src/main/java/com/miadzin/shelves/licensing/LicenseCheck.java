9aa95a68e036429555c4fa668c9b5c12b5b63f64
Kostyantyn Malakhov <info@varkon.biz>
2016-05-09 16:03:48 +0300

    First change

--- app/src/main/java/com/miadzin/shelves/licensing/LicenseCheck.java
+++ app/src/main/java/com/miadzin/shelves/licensing/LicenseCheck.java
@@ -7,19 +7,11 @@ import android.util.Log;
 
 public class LicenseCheck  {
     public static boolean check(Context context) {
-        if (BuildConfig.DEBUG)
-            return false;
-        PackageManager manager = context.getPackageManager();
-        return isUnlockerInstalled(context) && manager.checkSignatures("com.miadzin.shelves", "com.miadzin.shelves.unlocker") == PackageManager.SIGNATURE_MATCH;
+        return true;
     }
 
     public static boolean isUnlockerInstalled(Context context) {
-        PackageManager pm = context.getPackageManager();
-        try {
-            pm.getPackageInfo("com.miadzin.shelves.unlocker", PackageManager.GET_ACTIVITIES);
-            return true;
-        } catch (PackageManager.NameNotFoundException e) {
-            return false;
-        }
+                   return true;
+
     }
 }
